6fe54142426007b42651b0de896ed13a97472885
NancySDuguay <NancyDuguay@cunet.carleton.ca>
2017-02-02 08:57:56 -0500

    revert last commit

--- unrealProject/Source/unrealProject/FlowerGrow.cpp
+++ unrealProject/Source/unrealProject/FlowerGrow.cpp
@@ -61,7 +61,7 @@ void AFlowerGrow::Tick( float DeltaTime )
 
 void AFlowerGrow::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
-	//UE_LOG(LogTemp, Warning, TEXT("X %f"), this->MoveX);
+	UE_LOG(LogTemp, Warning, TEXT("X %f"), this->MoveX);
 	//UE_LOG(LogTemp, Warning, TEXT("X %f"), this->Scale);
 	//Destroy();
 	/*if (OtherActor->IsA(AunrealProjectCharacter::StaticClass()))
